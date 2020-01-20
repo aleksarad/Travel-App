be287495c1db1997f5034ce0caf6838558ef83a5
Aleksa <alexa.radevich1@gmail.com>
2020-01-20 13:25:09 -0500

    project directory setup

---
+++ src/client/index.js
@@ -0,0 +1,3 @@
+//should import/export main functions of our application js
+//import scss
+//If we are exporting functions from our application.js file, our event listeners can’t go there. Where can we put them? To call that exported function?
