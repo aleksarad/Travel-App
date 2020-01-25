2491bdad073583293426f4378cf3ebe18c2afe3a
Aleksa <alexa.radevich1@gmail.com>
2020-01-25 15:00:51 -0500

    cors anywhere setup

--- src/server/server.js
+++ src/server/server.js
@@ -12,6 +12,20 @@ app.use(cors());
 //Initializing main project folder
 app.use(express.static('dist'));
 
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: ['http://localhost:8000/'], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
+
 //Server setup
 app.listen(8000, () => { 
   console.log('Server running on http://localhost:8000/'); 
